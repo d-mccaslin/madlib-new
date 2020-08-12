ff2dc11e1d03adf6df128b008185d776caff7818
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-12 10:42:20 -0700

    fix input error in .js and got story to show
    Co-authored by: David McCaslin <davidmccaslin94@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    const person1Input = $("input#adjective1").val();
-    const person2Input = $("input#adjective2").val();
-    const typeOfFoodInput= $("input#typeOfFood").val();
-    const roomOfhouseInput= $("input#roomOfHouse").val();
+    // const person1Input = $("input#person1").val();
+    // const person2Input = $("input#person2").val();
+    // const animalInput= $("input#animal").val();
+    // const exclamationInput = $("input#exclamation").val();
+    // const verbInput = $("input#verb").val();
+    // const nounInput = $("input#noun").val();
+
+    // $(".person1").append(person1Input);
+    // $(".person2").append(person2Input);
+    // $(".animal").append(animalInput);
+    // $(".exclamation").append(exclamationInput);
+    // $(".verb").append(verbInput);
+    // $(".noun").append(nounInput);
+
+    const adjective1Input = $("input#adjective1").val();
+    const adjective2Input = $("input#adjective2").val();
+    const typeOfFoodInput = $("input#typeOfFood").val();
+    const roomOfHouseInput = $("input#roomOfHouse").val();
     const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb1").val();
-    const verbInput = $("input#verb2").val();
+    const verb1Input = $("input#verb1").val();
+    const verb2Input = $("input#verb2").val();
 
     $(".adjective1").text(adjective1Input);
     $(".adjective2").text(adjective2Input);
